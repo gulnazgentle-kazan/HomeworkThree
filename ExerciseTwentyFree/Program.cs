70e47b5b4e1c3cf04cc2b87a75fe9cddf8175ae7
gulnazgentle-kazan <gulnazvp@mail.ru>
2022-12-25 23:30:24 +0300

    did and added exercise 23

---
+++ ExerciseTwentyFree/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите любое число");
+double N = Convert.ToInt32(Console.ReadLine());
+
+for (double i = 1; i <= N; i++)
+{
+    double cube = Math.Pow(i, 3);
+    Console.WriteLine($"{cube}");
+}
+
